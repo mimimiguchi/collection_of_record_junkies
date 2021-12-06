18d5896c704768fbbc7cc78427b30791eb577b22
mimimiguchi <mimimiguchi@icloud.com>
2021-12-06 19:59:50 +0900

    ユーザー管理機能のマークアップ/スタイリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,19 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
 
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is valid. Include both letters and numbers'}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   validates :nickname, presence: true
-  validates :birthday, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'は有効でありません。全角文字で入力してください' } do
     validates :last_name
     validates :first_name
   end
 
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'は有効でありません。全角カタカナで入力してください' } do
     validates :last_name_kana
     validates :first_name_kana
   end
+
+  validates :birthday, presence: true
 
 end
