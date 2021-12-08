dbc092f78c29a3a2cac0c3d13ed3664fe7664f04
mimimiguchi <mimimiguchi@icloud.com>
2021-12-08 11:55:24 +0900

    モデル・テーブル作成、アソシエーション設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_04_053512) do
+ActiveRecord::Schema.define(version: 2021_12_08_021643) do
+
+  create_table "collections", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "music_id", null: false
+    t.string "title", null: false
+    t.string "artist", null: false
+    t.string "year"
+    t.text "description"
+    t.integer "size_id", null: false
+    t.integer "rpm_id", null: false
+    t.integer "status_id", null: false
+    t.integer "delivery_charge_id"
+    t.integer "prefecture_id"
+    t.integer "delivery_day_id"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_collections_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +49,5 @@ ActiveRecord::Schema.define(version: 2021_12_04_053512) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "collections", "users"
 end
