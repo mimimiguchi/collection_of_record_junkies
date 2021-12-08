c0b60e1a4fa4c42658618d9b44da73407118f3e4
mimimiguchi <mimimiguchi@icloud.com>
2021-12-08 20:23:10 +0900

    ビューファイルの作成

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,12 @@
 class CollectionsController < ApplicationController
   def index
   end
+
+  def new
+    @collection = Collection.new
+  end
+
+  def create
+  end
+  
 end
--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,27 @@
 class Collection < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :music_id, numericality: { other_than: 1 , message: "を選択してください。" }
+    validates :title
+    validates :artist
+    validates :size_id, numericality: { other_than: 1 , message: "を選択してください。" }
+    validates :rpm_id, numericality: { other_than: 1 , message: "を選択してください。" }
+    validates :status_id, numericality: { other_than: 1 , message: "を選択してください。" }
+  end
+
+  with_options presence: true, if: :sale? do
+    validates :delivery_charge_id, numericality: { other_than: 1 , message: "を選択してください。" }
+    validates :prefecture_id, numericality: { other_than: 1 , message: "を選択してください。" }
+    validates :delivery_day_id, numericality: { other_than: 1 , message: "を選択してください。" }
+    validates :price, numericality: { only_integer: true, message: "を整数で入力してください。" }
+  end
+
+
+  def sale?
+    status_id == 3
+  end
+
 end
