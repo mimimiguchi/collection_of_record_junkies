9f68ae84d95c0679dd2d1c5612bf12c9dcc54132
mimimiguchi <mimimiguchi@icloud.com>
2021-12-10 19:43:43 +0900

    一覧表示検索機能の修正

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,5 +1,5 @@
 class CollectionsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
 
   def index
     @collections = Collection.order("created_at DESC")
