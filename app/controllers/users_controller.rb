6739b6061300c645daf75aba67ea50a13e2e0db8
mimimiguchi <mimimiguchi@icloud.com>
2021-12-15 09:16:50 +0900

    個人投稿検索機能の修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :user_nickname, only: [:show, :search]
+
   def show
-    @user = User.find(params[:id])
-    @nickname = @user.nickname
     @collections = @user.collections
     @count_now = Collection.where(user_id: @user.id, status_id: 2).count
     @count_sell = Collection.where(user_id: @user.id, status_id: 3).count
@@ -9,7 +9,14 @@ class UsersController < ApplicationController
   end
 
   def search
-    @collections = Collection.search(params[:keyword])
+    @collections = Collection.where(user_id: @user.id).search(params[:keyword])
+  end
+
+  private
+
+  def user_nickname
+    @user = User.find(params[:id])
+    @nickname = @user.nickname
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: "collections#index"
 
   resources :users, only: :show do
-    collection do
+    member do
       get 'search'
     end
   end
