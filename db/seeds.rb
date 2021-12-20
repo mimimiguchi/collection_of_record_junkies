d15cf01fc9438f3e9dc6cb8400ce9bf3555c346b
mimimiguchi <mimimiguchi@icloud.com>
2021-12-20 12:00:59 +0900

    SEEDファイルによる投稿作業

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+collection = Collection.new(
+  music_id: 13,
+  title: "ASTEROID LOVE",
+  artist: "L-universe",
+  year: "",
+  description: "",
+  size_id: 3,
+  rpm_id: 2,
+  status_id: 2,
+  delivery_charge_id: 1,
+  prefecture_id: 1,
+  delivery_day_id: 1,
+  price: "",
+  user_id: 1
+)
+collection.image.attach(io: File.open(Rails.root.join('app/assets/images/IMG_2741.jpg')), filename: 'IMG_2741.jpg')
+collection.save!
+
